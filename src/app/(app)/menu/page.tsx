169d9e9656f829fefbe457b64c2568e1c91c62f1
Nataa <ardinata2408@gmail.com>
2025-12-12 08:58:45 +0000

    Pisahkan card ini dari card yang lain (_for element <CardHeader>_)

--- src/app/(app)/menu/page.tsx
+++ src/app/(app)/menu/page.tsx
@@ -261,7 +261,7 @@ export default function MenuPage() {
             />
             <Card className="rounded-xl">
               <CardHeader>
-                <div className="p-4 flex flex-col md:flex-row items-center gap-4">
+                <div className="flex flex-col md:flex-row items-center gap-4">
                   <div className="relative flex-grow w-full md:w-auto">
                     <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
                     <Input
@@ -308,7 +308,7 @@ export default function MenuPage() {
             />
             <Card className="rounded-xl">
               <CardHeader>
-                <div className="p-4 flex flex-col md:flex-row items-center gap-4">
+                <div className="flex flex-col md:flex-row items-center gap-4">
                   <div className="relative flex-grow w-full md:w-auto">
                     <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
                     <Input
