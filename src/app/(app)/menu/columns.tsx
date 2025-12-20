f62ea2e9b782de9872e490987f673c1b23e52354
M Farrel Arrahman <farrel@digits.id>
2025-12-20 01:11:47 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/app/(app)/menu/columns.tsx
+++ src/app/(app)/menu/columns.tsx
@@ -23,6 +23,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { useToast } from "@/hooks/use-toast"
 import Image from "next/image"
