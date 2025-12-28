e68855eed11ff0238923ffe285654f03870ab82f
M Farrel Arrahman <farrel@digits.id>
2025-12-28 03:52:53 +0000

    why is the app icon is still using 'S' from 'firebase studio' instead of

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,6 @@ export const metadata: Metadata = {
   title: 'Vamos POS',
   description: 'Vamos - Pool & Cafe Point of Sale',
   manifest: '/manifest.json',
-  icons: {
-    icon: '/icons/icon-192x192.png',
-    apple: '/icons/icon-192x192.png',
-  },
 };
 
 export default function RootLayout({
@@ -21,21 +17,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
+       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+        
+        {/* General Favicon */}
+        <link rel="icon" href="/logo.png" type="image/png" />
+        <link rel="shortcut icon" href="/logo.png" type="image/png" />
+
+        {/* Apple Touch Icon */}
+        <link rel="apple-touch-icon" href="/logo.png" />
+
+        {/* PWA Meta Tags */}
         <meta name="application-name" content="Vamos POS" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Vamos POS" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
-        <meta name="msapplication-TileColor" content="#FFFFFF" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#FFFFFF" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className="font-body antialiased">
         <AuthProvider>
